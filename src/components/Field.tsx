4955e5d9f00a6d7257ce996616ed34a719aa77b4
Carlos Gonzalez <homearanya@gmail.com>
2021-01-22 11:09:55 +0200

    initial value and card style

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -8,6 +8,7 @@ import {
   Button,
   Icon,
   CheckboxField,
+  Card,
 } from "@contentful/forma-36-react-components"
 import {
   SortableContainer,
@@ -46,81 +47,94 @@ const SortableList = SortableContainer(
     <div>
       {items.map((item, index) => (
         <SortableItem key={`${item.title}-${index}`} index={index}>
-          <Flex marginBottom="spacingS">
-            <Flex alignSelf="stretch" marginRight="spacingS">
-              <DragHandle />
-            </Flex>
-            <Flex marginRight="spacingS">
-              <Flex marginBottom="spacingS" flexDirection="column">
-                <FormLabel htmlFor={`title-${index}`}>Title</FormLabel>
-                <TextInput
-                  required
-                  name={`title-${index}`}
-                  id={`title-${index}`}
-                  placeholder="Title"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    updateItems([
-                      ...items.slice(0, index),
-                      { ...item, title: e.target.value },
-                      ...items.slice(index + 1),
-                    ])
-                  }
-                  value={item.title}
-                />
-              </Flex>
-            </Flex>
+          <Flex marginBottom="spacingS" flexDirection="column">
+            <Card padding="none">
+              <Flex>
+                <Flex alignSelf="stretch" marginRight="spacingS">
+                  <DragHandle />
+                </Flex>
+                <Flex
+                  flexGrow={1}
+                  marginRight="spacingS"
+                  marginTop="spacingS"
+                  marginBottom="spacingS"
+                >
+                  <Flex marginRight="spacingS">
+                    <Flex marginBottom="spacingS" flexDirection="column">
+                      <FormLabel htmlFor={`title-${index}`}>Title</FormLabel>
+                      <TextInput
+                        required
+                        name={`title-${index}`}
+                        id={`title-${index}`}
+                        placeholder="Title"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          updateItems([
+                            ...items.slice(0, index),
+                            { ...item, title: e.target.value },
+                            ...items.slice(index + 1),
+                          ])
+                        }
+                        value={item.title}
+                      />
+                    </Flex>
+                  </Flex>
 
-            <Flex flexGrow={1} flexDirection="column">
-              <FormLabel htmlFor={`content-${index}`}>Content</FormLabel>
-              <Textarea
-                name={`content-${index}`}
-                id={`content-${index}`}
-                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-                  updateItems([
-                    ...items.slice(0, index),
-                    { ...item, content: e.target.value },
-                    ...items.slice(index + 1),
-                  ])
-                }
-                value={item.content}
-                rows={1}
-              />
-            </Flex>
-            <Flex
-              marginLeft="spacingM"
-              flexDirection="column"
-              alignItems="center"
-            >
-              <FormLabel htmlFor="hide">Hide</FormLabel>
-              <Flex marginLeft="spacingXs">
-                <CheckboxField
-                  id="hide"
-                  name="hide"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    updateItems([
-                      ...items.slice(0, index),
-                      { ...item, hide: e.target.checked },
-                      ...items.slice(index + 1),
-                    ])
-                  }
-                  checked={item.hide}
-                  labelText=""
-                />
+                  <Flex flexGrow={1} flexDirection="column">
+                    <FormLabel htmlFor={`content-${index}`}>Content</FormLabel>
+                    <Textarea
+                      name={`content-${index}`}
+                      id={`content-${index}`}
+                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                        updateItems([
+                          ...items.slice(0, index),
+                          { ...item, content: e.target.value },
+                          ...items.slice(index + 1),
+                        ])
+                      }
+                      value={item.content}
+                      rows={1}
+                    />
+                  </Flex>
+                  <Flex
+                    marginLeft="spacingM"
+                    flexDirection="column"
+                    alignItems="center"
+                  >
+                    <FormLabel htmlFor="hide">Hide</FormLabel>
+                    <Flex marginLeft="spacingXs">
+                      <CheckboxField
+                        id="hide"
+                        name="hide"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                          updateItems([
+                            ...items.slice(0, index),
+                            { ...item, hide: e.target.checked },
+                            ...items.slice(index + 1),
+                          ])
+                        }
+                        checked={item.hide}
+                        labelText=""
+                      />
+                    </Flex>
+                  </Flex>
+                  <Flex
+                    marginLeft="spacingM"
+                    flexDirection="column"
+                    alignItems="center"
+                  >
+                    <FormLabel htmlFor={`content-${index}`}>Remove</FormLabel>
+                    <div
+                      onClick={() =>
+                        updateItems(items.filter((e, i) => i !== index))
+                      }
+                      style={{ marginTop: "4px" }}
+                    >
+                      <Icon icon="Close" style={{ cursor: "pointer" }} />
+                    </div>
+                  </Flex>
+                </Flex>
               </Flex>
-            </Flex>
-            <Flex
-              marginLeft="spacingM"
-              flexDirection="column"
-              alignItems="center"
-            >
-              <FormLabel htmlFor={`content-${index}`}>Remove</FormLabel>
-              <div
-                onClick={() => updateItems(items.filter((e, i) => i !== index))}
-                style={{ marginTop: "4px" }}
-              >
-                <Icon icon="Close" style={{ cursor: "pointer" }} />
-              </div>
-            </Flex>
+            </Card>
           </Flex>
         </SortableItem>
       ))}
@@ -129,7 +143,9 @@ const SortableList = SortableContainer(
 )
 
 const Field = (props: FieldProps) => {
-  const [items, setItems] = useState<Array<Item>>(props.sdk.field.getValue())
+  const [items, setItems] = useState<Array<Item>>(
+    props.sdk.field.getValue() || []
+  )
 
   const updateItems = (newItems: Item[]) => {
     setItems(newItems)
